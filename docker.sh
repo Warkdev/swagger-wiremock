a80aeda53b47546dd61a90f60f5a9c8f4c13b531
Warkdev <cedric.servais@outlook.com>
2022-11-06 22:17:45 +0100

    Add config for axway_cft versions

--- docker.sh
+++ docker.sh
@@ -1,3 +1,8 @@
-docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v332 -t stoplight/prism mock -h 0.0.0.0 /root/apis/swagger-332.yml
-docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v35 -t stoplight/prism mock -h 0.0.0.0 /root/apis/swagger-35.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v332 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-332.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v35 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-35.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v36 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-36.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v37 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-37.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v38 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-38.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v39 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-39.yml
+docker run -it --rm -v ~/swagger-wiremock:/root/apis --name prism_axway_cft_v310 -t stoplight/prism mock -h 0.0.0.0 /root/apis/openapi-310.yml
 docker run -it -d -p 8443:443 -v ~/swagger-wiremock/nginx:/etc/nginx/conf.d:ro --name web_proxy nginx
